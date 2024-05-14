c8e279c5ea741275d615ce1cabfe2063c6a80f75
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-05-14 01:33:52 +0000

    finished

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,7 +6,9 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :string
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
+
 end
